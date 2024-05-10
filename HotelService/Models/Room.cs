0254be77b95ec22eb77e01dcec02362be03e5492
Tomash Mikulevich <tommikulevich@gmail.com>
2024-05-10 21:10:19 +0200

    Little fixes in hotel models.

--- HotelService/Models/Room.cs
+++ HotelService/Models/Room.cs
@@ -4,15 +4,8 @@ namespace HotelService.Models {
         public Guid HotelId { get; set; }
         public int NumOfPeople { get; set; }
         public int NumOfRooms { get; set; }
-        public RoomType RoomType { get; set; }
+        public string RoomType { get; set; }
         public string Features { get; set; }
         public List<RoomEvent> RoomEvents { get; set; }
-    }
-
-    public enum RoomType {
-        Studio,
-        Single,
-        Double,
-        Triple
     }
 }
--- HotelService/Models/RoomEvent.cs
+++ HotelService/Models/RoomEvent.cs
@@ -2,14 +2,8 @@ namespace HotelService.Models {
     public class RoomEvent {
         public Guid Id { get; set; }
         public Guid RoomId { get; set; }
-        public RoomEventStatus Status { get; set; }
+        public string Status { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-    }
-
-    public enum RoomEventStatus {
-        Available,
-        Pending,
-        Reserved
     }
 }
